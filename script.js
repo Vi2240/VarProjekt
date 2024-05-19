47a63b6501f33df16c61c9eb7b1f963e903bc455
Vi2240 <vincentivarsson27@gmail.com>
2024-05-19 18:41:39 +0200

    Går typ att köpa

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ for (var i = 0; i < btns.length; i++) {
       this.className += " active";
     });
   }
+
+  function BuyCar(car) {
+
+    if(car == 1)
+    {
+      carName = "BMW M4";
+    }
+    else if(car == 2)
+    {
+      carName = "Mercedes-BenzAMG GT";
+    }
+    else if(car == 3)
+    {
+      carName = "BMW i8";
+    }
+    else if(car == 4)
+    {
+      carName = "Rimac c two";
+    }
+    else if(car == 5)
+    {
+      carName = "Ferrari Enzo"
+    }
+    else if(car == 6)
+    {
+      carName = "Bugatti la voiture noire"
+    }
+
+    let purchasedCar = confirm("Do you want to purchas " + carName)
+
+    if(purchasedCar){
+      alert("Congratulations you have purchased " + carName);
+    }
+    else{
+      alert("Sad that you did not purchas " + carName)
+    }
+  }
